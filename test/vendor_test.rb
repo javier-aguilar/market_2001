d4a75314e649678170467ad7a9d03514f89889b4
Javier Aguilar <jvr.agx@gmail.com>
2020-02-26 10:21:11 -0700

    Pass test_it_can_stock

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -1,16 +1,29 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/item'
 require './lib/vendor'
 
 class VendorTest < Minitest::Test
 
-  def test_it_exists
-
-    assert_instance_of
+  def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @vendor = Vendor.new("Rocky Mountain Fresh")
   end
 
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
 
   def test_it_has_attributes
-
+    expected = {}
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+    assert_equal expected, @vendor.inventory
   end
+
+  def test_it_can_stock
+    assert_equal 0, @vendor.check_stock(@item1)
+    @vendor.stock(@item1, 30)
+    assert_equal 30, @vendor.check_stock(@item1)
+  end
+
 end
