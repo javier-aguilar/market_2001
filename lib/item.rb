cd6c80d3c92daaee2985473ecebda29e99c8174f
Javier Aguilar <jvr.agx@gmail.com>
2020-02-26 10:11:48 -0700

    Update assertion test for price attribute

--- lib/item.rb
+++ lib/item.rb
@@ -4,7 +4,11 @@ class Item
 
   def initialize(item_info)
     @name = item_info[:name]
-    @price = item_info[:price]
+    @price = parse_price(item_info[:price])
+  end
+
+  def parse_price(price)
+    price.to_s.tr('$', '').to_f
   end
 
 end
--- test/item_test.rb
+++ test/item_test.rb
@@ -6,6 +6,7 @@ class ItemTest < Minitest::Test
 
   def setup
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
 
   def test_it_exists
@@ -13,7 +14,7 @@ class ItemTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_equal "Peach", @item1.name
-    assert_equal "$0.75", @item1.price
+    assert_equal "Tomato", @item2.name
+    assert_equal 0.50, @item2.price
   end
 end
