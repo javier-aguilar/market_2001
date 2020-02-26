b9da96a72461c0b55de54d36423182059de707b8
Javier Aguilar <jvr.agx@gmail.com>
2020-02-26 10:57:43 -0700

    Pass test_it_can_return_vendors_that_sell_specific_item

---
+++ lib/market.rb
@@ -0,0 +1,26 @@
+class Market
+
+  attr_reader :name, :vendors
+
+  def initialize(name)
+    @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map {|vendor| vendor.name}
+  end
+
+  def vendors_that_sell(item)
+    @vendors.find_all do |vendor|
+      vendor.inventory.find do | items |
+        items.first == item
+      end
+    end
+  end
+
+end
