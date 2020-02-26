4f251795026d65f563d4391d38c6fa9e34157e9a
Javier Aguilar <jvr.agx@gmail.com>
2020-02-26 10:08:11 -0700

    Pass item test exists and attributes

---
+++ test/market_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/vendor'
+
+class VendorTest < Minitest::Test
+
+  def setup
+    skip
+  end
+
+  def test_it_exists
+    assert_instance_of
+  end
+
+
+  def test_it_has_attributes
+
+  end
+end
---
+++ test/vendor_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+
+class VendorTest < Minitest::Test
+
+  def test_it_exists
+
+    assert_instance_of
+  end
+
+
+  def test_it_has_attributes
+
+  end
+end
